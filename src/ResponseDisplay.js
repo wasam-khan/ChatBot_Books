85efdb491fdc57961a3c2f172f9357d5891f3cc8
DESKTOP-J6QB9Q1\wasam <Devpc01@connexus-technologies.com>
2024-07-30 11:39:28 +0500

    commit

--- src/ResponseDisplay.js
+++ src/ResponseDisplay.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './ResponseDisplay.css';
 import { useState, useEffect } from 'react';
+import svg from './speaker-sketch-loud-volume-interface-tool-svgrepo-com.svg';
+
+
+
 
 function ResponseDisplay({ reference, response , book }) {
     const [displayedText, setDisplayedText] = useState("");
+    const [isSpeaking, setIsSpeaking] = useState(false);
+
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
@@ -16,11 +22,36 @@ function ResponseDisplay({ reference, response , book }) {
         }
       }, [index, response]);
 
+      const handleSpeak = () => {
+        if ('speechSynthesis' in window) {
+          if (isSpeaking) {
+            window.speechSynthesis.cancel();
+            setIsSpeaking(false);
+          } else {
+            const utterance = new SpeechSynthesisUtterance(response);
+            // utterance.lang = language;
+            // console.log(utterance.lang);
+            window.speechSynthesis.speak(utterance);
+            setIsSpeaking(true);
+            
+            // Reset the isSpeaking flag when the speech ends
+            utterance.onend = () => {
+              setIsSpeaking(false);
+            };
+          }
+        } else {
+          alert('Sorry, your browser does not support text to speech!');
+        }
+      }
+
 
     return (
         <div className="response-display">
         <h2>Response</h2>
         <p>{displayedText}</p>
+        <button onClick={handleSpeak}>
+          <img src={svg} alt="Speak" style={{ width: '18px', height: '18px' }} />
+        </button>
         {index === response.length && reference && (
           <p className="reference-link">
             <a href={reference} target="_blank" rel="noopener noreferrer">
